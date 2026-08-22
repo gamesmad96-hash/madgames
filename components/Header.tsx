963a7547d791b166caf8633b2335088b0316b681
gamesmad96-hash <gamesmad96@gmail.com>
2026-08-23 04:24:12 +0530

    seo: connect hot and new navigation to discovery pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const nav = [
-  ['◉','Home','/'],['♨','Hot','/'],['✣','New','/'],['♞','Action','/category/action'],
+  ['◉','Home','/'],['♨','Hot','/hot-games'],['✣','New','/new-games'],['♞','Action','/category/action'],
   ['⚔','Adventure','/category/adventure'],['♙','Casual','/category/casual'],['⚄','Racing','/category/racing'],
   ['▧','Puzzle','/category/puzzle'],['✦','Sports','/category/sports'],['⌾','Recent','/recent']
 ] as const;
