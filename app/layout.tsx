22b04e5a0a47c22cd568730b5bc4be11707b1e3a
gamesmad96-hash <gamesmad96@gmail.com>
2026-09-12 02:21:32 +0530

    Add Google AdSense script globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,9 @@ export default function RootLayout({children}:{children:React.ReactNode}){
       }
     ]
   };
-  return <html lang="en"><body>
+  return <html lang="en"><head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9164731215807465" crossOrigin="anonymous"/>
+  </head><body>
     <script type="application/ld+json" dangerouslySetInnerHTML={{__html:JSON.stringify(identityLd)}}/>
     {gaId&&<>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="lazyOnload"/>
